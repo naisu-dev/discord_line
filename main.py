a443b2eb15585248ccefac40c7498cbbc7889062
nice <118091656+naisu-dev@users.noreply.github.com>
2023-01-15 17:51:16 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,29 @@
+import os
+import requests
+from dotenv import load_dotenv
+import discord
+
+load_dotenv()
+
+bot = discord.Client(intents=discord.Intents.all())
+
+
+@bot.event
+async def on_message(message):
+  if message.author.bot:
+    return
+  else:
+    headers = {
+        'Authorization': 'Bearer ' + os.environ["notify_url"],
+    }
+    files = {
+        'message': (None, "<" + message.author.name + ">" + message.content),
+    }
+    response = requests.post('https://notify-api.line.me/api/notify',
+                             headers=headers,
+                             files=files)
+
+
+TOKEN = os.environ["discord_token"]
+
+bot.run(TOKEN)
